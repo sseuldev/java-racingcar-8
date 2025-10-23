7c286ac6a7cebe26952d4f3548ebc8edbbce3e08
sseuldev <lhs010107@gmail.com>
2025-10-24 03:49:03 +0900

    feat: Application 구현 객체 조립 역할을 담당하는 Application 흐름을 구현했다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import racingcar.config.RacingConfig;
+import racingcar.controller.RacingController;
+import racingcar.controller.ReadyController;
+import racingcar.domain.Race;
+import racingcar.helper.PreparationHelper;
+import racingcar.helper.RandomNumberHelper;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        PreparationHelper preparationHelper = new PreparationHelper();
+        RandomNumberHelper randomNumberHelper = new RandomNumberHelper();
+
+        ReadyController readyController = new ReadyController(inputView, preparationHelper);
+        RacingConfig racingConfig = readyController.ready();
+
+        Race race = new Race(racingConfig.cars(), randomNumberHelper);
+
+        RacingController racingController = new RacingController(outputView, racingConfig, race);
+        racingController.start();
     }
 }
