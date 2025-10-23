31a890a7469360ca7bc85907a1c37c1cc528ec49
sseuldev <lhs010107@gmail.com>
2025-10-24 02:30:31 +0900

    feat(domain): Car 클래스 구현 자동차 자신의 이름과 전진 거리에 대한 상태를 관리하는 클래스를 구현했다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,45 @@
+package racingcar.domain;
+
+import racingcar.exception.ErrorMessages;
+
+public class Car {
+
+    private static final int MAXIMUM_CAR_LENGTH = 5;
+
+    private final String name;
+    private int distance = 0;
+
+    public Car(String name){
+        validateName(name);
+        this.name = name;
+    }
+
+    public void move() {
+        distance++;
+    }
+
+    private void validateName(String name) {
+        validateNameEmpty(name);
+        validateNameLength(name);
+    }
+
+    private void validateNameEmpty(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_CAR_NAME_EMPTY);
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAXIMUM_CAR_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_CAR_NAME_LENGTH);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
